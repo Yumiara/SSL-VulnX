95dc6795baec560a2650a0465d2c0e70bdd97933
Yumiria <154666142+Yumiara@users.noreply.github.com>
2025-11-30 13:18:02 +0700

    Update M.lua

--- APIs/M.lua
+++ APIs/M.lua
@@ -21940,12 +21940,12 @@ end;
 
 local AC_Call, AC_Error = pcal(function(): boolean
     if AlreadyLoadACI then return true; end;
-    local ACI:(any)->({}),ACI_Source:nil = loadScriptFromCache("https://raw.githubusercontent.com/Yumiara/SSL-VulnX/refs/heads/main/APIs/MultiAC.cpp", "MultiAC.cpp", false, 1000000, false), nil;
+    local ACI:(any)->({}),ACI_Source:nil = loadScriptFromCache("https://raw.githubusercontent.com/Yumiara/SSL-VulnX/refs/heads/main/APIs/MultiAC.cpp", "MultiAC.lua", false, 1000000, false), nil;
     if not ACI then return Kick(selff, "BYPASSING Failed, Contact Script Devs. [1]"); end;
     if ACI.Version ~= "2023_ACI_2025_2" then
         ACI_Source = HttpGet(game, "https://raw.githubusercontent.com/Yumiara/SSL-VulnX/refs/heads/main/APIs/MultiAC.cpp")(); ACI = loadstring(ACI_Source);
         if ACI.Version ~= "2023_ACI_2025_2" then return Kick(selff, "BYPASSING Failed, Contact Script Devs. [2]"); end;
-    end; if LoaderSettings.AllowCache and ACI_Source then writefile("FlowXS/MultiAC.cpp", ACI_Source); end;
+    end; if LoaderSettings.AllowCache and ACI_Source then writefile("FlowXS/MultiAC.lua", ACI_Source); end;
     return ACI.Function();
 end); if not AC_Call then return Kick(selff, "BYPASSING Failed, Contact Script Devs. [3]"), setc(AC_Error); end;
 
